d484b498404f021e41b77558f82c9a2e5b0117d2
SvenLang <SvenLang07@gmail.com>
2017-02-27 17:13:11 +0100

    use single quote

--- example/nodeCordovaTools.js
+++ example/nodeCordovaTools.js
@@ -1,4 +1,4 @@
-'use strict';
+#!/usr/bin/env node
 
 /**
  * @author Sven Heddtröm-Lang
@@ -6,49 +6,49 @@
 
 module.exports = {
 
-	config: "www/js/Project/app/www/config.json", // todo
-	delete_projectPath: true, // todo
+	configJs: 'www/js/Project/app/www/config.js', // todo
 
 	bundleId: 'com.apuerto.palmeras',
 	title: 'Fonda Las Palmeras',
-	projectPath: "build", // todo solution create folder when '/', mkdir, rm -rf ?
+	version: '1.2.4',
+	projectPath: 'build', // todo solution create folder when '/', mkdir, rm -rf ?
 	platforms: [
-		"browser",
-		"android"
+		'browser',
+		'android'
 	],
 	plugins: [
-		"cordova-plugin-device",
-		"cordova-plugin-dialogs",
-		"cordova-plugin-whitelist"
+		'cordova-plugin-device',
+		'cordova-plugin-dialogs',
+		'cordova-plugin-whitelist'
 	],
-	sassInput: "www/css/_index.scss",
-	sassOutput: "www/css/index.css", // relativ to projectPath
+	sassInput: 'www/css/_index.scss',
+	sassOutput: 'www/css/index.css', // relativ to projectPath
 	copy: {
 		// input: output // output = relativ to projectPath
-		"www/index.html": "www/index.html",
-		"www/js/Project/app/www/config.json": "www/config.json",
-		"www/js/Project/app/www/favicon.ico": "www/favicon.ico",
-		"www/js/Project/app/www/img": "www",
-		"www/js/Project/app/platforms": ""
+		'www/index.html': 'www/index.html',
+		'www/js/Project/app/www/config.json': 'www/config.json',
+		'www/js/Project/app/www/favicon.ico': 'www/favicon.ico',
+		'www/js/Project/app/www/img': 'www',
+		'www/js/Project/app/platforms': ''
 	},
-	eslintrc: ".eslintrc.js",
+	eslintrc: '.eslintrc.js',
 	eslintFiles: [
-		"www/js/App.js",
-		"www/js/**/*.js",
-		"www/js/Project/www/js/ProjectModule.js",
-		"www/js/Project/www/js/ProjectPlugin.js"
+		'www/js/App.js',
+		'www/js/**/*.js',
+		'www/js/Project/www/js/ProjectModule.js',
+		'www/js/Project/www/js/ProjectPlugin.js'
 	],
 	jsMinifyOptions: {
-		compressor: "uglifyjs",
+		compressor: 'uglifyjs',
 		input: [
-			"_tmp/*.js"
+			'_tmp/*.js'
 		],
-		output: "www/js/bar.js" // relativ to projectPath
+		output: 'www/js/bar.js' // relativ to projectPath
 	},
 	jsBrowserify: {
-		input: "www/js/App.js",
-		output: "www/js/index.js", // relativ to projectPath
-		options: "--plugins [] --presets [ es2015 ]"
+		input: 'www/js/App.js',
+		output: 'www/js/index.js', // relativ to projectPath
+		options: '--plugins [] --presets [ es2015 ]'
 	}
 
 };
