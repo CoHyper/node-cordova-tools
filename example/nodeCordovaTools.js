99d202a3347ba6349e258938f2f1e3f94e61761e
SvenLang <SvenLang07@gmail.com>
2017-02-27 05:52:45 +0100

    add palmeras config

--- example/nodeCordovaTools.js
+++ example/nodeCordovaTools.js
@@ -6,29 +6,45 @@
 
 module.exports = {
 
-	config: "www/js/Project/app/www/config.json",
+	// todo need root ? - no
 
-	bundleId: 'com.apuerto.myyapp',
-	title: 'Myy App',
-	projectPath: "_build", // todo solution create folder when '/', mkdir, rm -rf
+	config: "www/js/Project/app/www/config.json", // todo
+
+	bundleId: 'com.apuerto.palmeras',
+	title: 'Fonda Las Palmeras',
+	projectPath: "build", // todo solution create folder when '/', mkdir, rm -rf ?
 	platforms: [
 		"browser",
-		"android",
-		"ios"
+		"android"
 	],
 	plugins: [
 		"cordova-plugin-device",
-		"cordova-plugin-dialogs"
+		"cordova-plugin-dialogs",
+		"cordova-plugin-whitelist"
 	],
-	sassInput: "app/www/css/_index.scss",
+	sassInput: "www/css/_index.scss",
 	sassOutput: "www/css/index.css", // relativ to projectPath
 	copy: {
 		// relativ to projectPath
 		"www/index.html": "www/index.html",
-		"www/js/Project/app/www/favicon.ico": "www/favicon.ico",
 		"www/js/Project/app/www/config.json": "www/config.json",
+		"www/js/Project/app/www/favicon.ico": "www/favicon.ico",
 		"www/js/Project/app/www/img": "www",
 		"www/js/Project/app/platforms": ""
+	},
+	eslintrc: "./.eslintrc.js",
+	eslintFiles: [
+		"www/js/App.js",
+		"www/js/**/*.js",
+		"www/js/Project/www/js/ProjectModule.js",
+		"www/js/Project/www/js/ProjectPlugin.js"
+	],
+	jsMinifyOptions: {
+		compressor: "uglifyjs",
+		input: [
+			"_tmp/*.js"
+		],
+		output: "www/js/bar.js" // relativ to projectPath
 	}
 
 };
