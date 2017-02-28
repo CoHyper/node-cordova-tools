9f4fcafd3da1d57fa4936eefcf215a1efc433837
SvenLang <SvenLang07@gmail.com>
2017-02-28 13:52:22 +0100

    p

--- example/nct.config.js
+++ example/nct.config.js
@@ -10,8 +10,9 @@ module.exports = {
 	 * If this set : all other variable ignore.
 	 */
 	// configJS: 'node-cordova-tools/tests/nct.config,js',
-	//configJS: 'tests/nct.config,js',
-	configJS: '../tests/nct2.config,js',
+	// configJS: 'tests/nct.config,js',
+	// configJS: '../tests/nct2.config.js',
+	configJS: '../../tests/nct2.config.js',
 
 	/**
 	 * @type string
