ed83de2ee524575358f18c0c26b7496a661c94b9
SvenLang <SvenLang07@gmail.com>
2017-02-28 23:59:17 +0100

    update licence

--- src/android-install-apk-release.js
+++ src/android-install-apk-release.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
 
+/*
+ * node-cordova-tools
+ * https://github.com/CoHyper/node-cordova-tools
+ *
+ * Copyright (c) 2017 Sven Hedström-Lang
+ * Licensed under the MIT license.
+ */
+
 let exec = require('child_process').exec;
 let fs = require('fs');
 let CONFIG = require('./../lib/config');
 
 /**
- * @author Sven Hedström-Lang
  *
  * @requires "Android Device to USB"
  *
--- src/cordova-plugins-remove.js
+++ src/cordova-plugins-remove.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 
+/*
+ * node-cordova-tools
+ * https://github.com/CoHyper/node-cordova-tools
+ *
+ * Copyright (c) 2017 Sven Hedström-Lang
+ * Licensed under the MIT license.
+ */
+
 let exec = require('child_process').exec;
 let fs = require('fs');
 let CONFIG = require('./../lib/config');
