b25ca4be6c0ed33faf54e4ed51e1a45e63dd6b8d
SvenLang <SvenLang07@gmail.com>
2017-02-28 13:55:06 +0100

    u

--- example/nct.config.js
+++ example/nct.config.js
@@ -12,7 +12,8 @@ module.exports = {
 	// configJS: 'node-cordova-tools/tests/nct.config,js',
 	// configJS: 'tests/nct.config,js',
 	// configJS: '../tests/nct2.config.js',
-	configJS: '../../tests/nct2.config.js',
+	// configJS: '../../tests/nct2.config.js',
+	configJS: 'tests/nct.config,js',
 
 	/**
 	 * @type string
